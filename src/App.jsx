b0c62266533e90321c3b872694fb94bfd08fe649
Monkee45 <michael.tom.walsh@gmail.com>
2025-12-14 14:09:48 +1100

    added a loading... message

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
+import Loading from "./Loading"
 
 const url = "https://www.course-api.com/react-tours-project";
 
 function App() {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
  
   useEffect( () => { 
     fetchTours() 
   },[]) 
   
   async function fetchTours() { 
+    setLoading(true)
     let response = await fetch(url); 
     let data = await response.json(); 
     setTours(data); 
+    setLoading(false)
   }
   
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
   return (
     <div>
     {tours.map((tour) => (
