873fcb5aa4c1ab793d5e5030808151629e1a6448
Monkee45 <michael.tom.walsh@gmail.com>
2025-12-15 12:36:28 +1100

    added read more/show less buttons

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import {useState} from "react";
 
 const Tour = (props) => {
+	const [readMore, setReadMore] = useState(true)
 	const {id, image, info, price, name} = props;
+
 	return (
 		<article className="single-tour">
 			<img src={image} alt={name} />
@@ -10,7 +13,12 @@ const Tour = (props) => {
 					<h4>{name}</h4>
 					<h4 className="tour-price">{price}</h4>
 				</div>
-				<p>{info}</p>
+				<p>
+					{ readMore ? info.substring(0, 200) : info }
+					<button onClick={() => setReadMore(!readMore)}>
+						{readMore ? "read more": "show Less" }
+					</button>
+				</p>
 				<button className="delete-btn">Not Interested</button>
 			</footer>
 		</article>
